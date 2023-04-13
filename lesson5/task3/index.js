0f6d567773bb8c6ec7b73866aab39a4bb96e49eb
Oleksii Ulianov <alexallorayz@gmail.com>
2023-04-13 21:07:44 +0200

    Even/Odd function

---
+++ lesson5/task3/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// put your code here
+function getEvenOdd(number) {
+	if (number % 2 === 0) {
+		return 'even';
+	} else {
+		return 'odd';
+	}
+}
+
+console.log(getEvenOdd(8));
+console.log(getEvenOdd(9));
