a8e54a14098ad0179a23699fdffcc108becd9ed2
Oleksii Ulianov <alexallorayz@gmail.com>
2023-04-13 21:52:29 +0200

    Кратные на промежутке

---
+++ lesson5/HWTask4/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// put your code here
+function findDivCount(a, b, n) {
+	let count = 0;
+	for (let i = a; i <= b; i += 1) {
+		if (i % n === 0) {
+			count += 1;
+		}
+	}
+	return count;
+}
