5fe9d5ab41a48f681a9fb0221ca37d301e18f717
Oleksii Ulianov <alexallorayz@gmail.com>
2023-05-01 11:30:00 +0200

    Фильтр имен

---
+++ lesson 7/HomeTask7/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const filterNames = (arr, text) =>
+	arr.filter(name => name.includes(text) && name.length > 5);
+
+console.log(
+	filterNames(
+		['John', 'Olivya', 'Olexandr', 'Emanuel', 'Vanya', 'Nastya'],
+		'ya'
+	)
+);
