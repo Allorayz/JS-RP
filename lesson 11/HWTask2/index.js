2c1b0ec9189f24b81b0112d37d5364d7b8a98586
Oleksii Ulianov <alexallorayz@gmail.com>
2023-05-14 14:55:44 +0200

    Count occurences

--- lesson 11/HWTask2/index.js
+++ lesson 11/HWTask2/index.js
@@ -1,7 +1,15 @@
-const countOccurrences = (text, str) => {
-	if (text === !String) {
+const countOccurrences = (str1, str2) => {
+	if (str2 === '') {
 		return null;
 	}
-};
 
-console.log(countOccurrences(1));
+	let count = 0;
+	let position = str1.indexOf(str2);
+	// console.log(position);
+	while (position !== -1) {
+		console.log('#index ' + position);
+		count += 1;
+		position = str1.indexOf(str2, position + 1);
+	}
+	return count;
+};
