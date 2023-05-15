0ecb46d3a1292a1be3624f886fdd8b08b7cae0a1
Oleksii Ulianov <alexallorayz@gmail.com>
2023-05-15 11:15:20 +0200

    Очистка данных о транзакциях

--- lesson 12/HWTask2/index.js
+++ lesson 12/HWTask2/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // put your code here
-const cleanTransactionList = transactions =>
+const cleanTransactionsList = transactions =>
 	transactions.filter(el => el >= 0).map(el => '$' + Number(el).toFixed(2));
 
 console.log(cleanTransactionList(['    1.9', '16.4', 17, ' 1 d', 0]));
