a38a1d8f6e7fea4ce771cee28f7c1a9956f8d02e
Oleksii Ulianov <alexallorayz@gmail.com>
2023-05-18 18:20:33 +0200

    Калькулятор с памятью

---
+++ lesson14/task3/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let memory = 0;
+
+export function add(num) {
+	memory += num;
+}
+
+export function decrease(num) {
+	memory -= num;
+}
+
+export function reset(num) {
+	memory = 0;
+}
+
+export function getMemo(num) {
+	return memory;
+}
