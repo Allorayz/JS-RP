b0c9c0be8cb6a92d3d1f628a6d0fb7bf793f1ebe
Oleksii Ulianov <alexallorayz@gmail.com>
2023-05-21 14:15:59 +0200

    Полное имя

---
+++ lesson17/HWTask4/index.js
@@ -0,0 +1,13 @@
+const user = {
+	firstName: 'John',
+	lastName: 'Doe',
+	getFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	},
+	setFullName(fullName) {
+		this.firstName = fullName.split(' ')[0];
+		this.lastName = fullName.split(' ')[1];
+	},
+};
+
+export { user };
