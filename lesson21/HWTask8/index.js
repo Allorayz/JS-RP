d793a1ed01168874ee103f1c5513453a338c748a
Oleksii Ulianov <alexallorayz@gmail.com>
2023-07-07 09:52:02 +0200

    Создание DOM элементов и вставка на страницу - 2

---
+++ lesson21/HWTask8/index.js
@@ -0,0 +1,19 @@
+export function finishList() {
+	const firstItem = document.createElement('li');
+	firstItem.textContent = '1';
+	document.querySelector('.list').prepend(firstItem);
+
+	const fourthItem = document.createElement('li');
+	fourthItem.textContent = '4';
+	document.querySelector('.special').before(fourthItem);
+
+	const sixthItem = document.createElement('li');
+	sixthItem.textContent = '6';
+	document.querySelector('.special').after(sixthItem);
+
+	const eightItem = document.createElement('li');
+	eightItem.textContent = '8';
+	document.querySelector('.list').append(eightItem);
+}
+
+finishList();
