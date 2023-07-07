cabfb5634a8c0af4d9f10e434bd2d9e6340641b5
Oleksii Ulianov <alexallorayz@gmail.com>
2023-07-07 10:05:30 +0200

    Установка атрибутов для DOM элементов

---
+++ lesson21/HWTask9/index.js
@@ -0,0 +1,11 @@
+export function finishForm() {
+	const setLogin = document.querySelector('input');
+	setLogin.setAttribute('name', 'login');
+
+	const newInput = document.createElement('input');
+	const setNewInput = document.querySelector('.login-form');
+	setNewInput.append(newInput);
+	newInput.setAttribute('type', 'password');
+}
+
+finishForm();
