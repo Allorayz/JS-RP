9460a0d21be0f83379509065bd81bee84ad4067f
Oleksii Ulianov <alexallorayz@gmail.com>
2023-07-08 09:32:49 +0200

    Дата атрибуты (data-attributes)

---
+++ lesson21/HWTask11/index.js
@@ -0,0 +1,8 @@
+export function squaredNumbers() {
+	const number = document.querySelectorAll('.number');
+	number.forEach(function (num) {
+		num.dataset.squaredNum = num.dataset.number * num.dataset.number;
+	});
+}
+
+squaredNumbers();
